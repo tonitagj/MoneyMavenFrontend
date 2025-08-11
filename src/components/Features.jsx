d1e2545fb8ff7ce8a15afc778848e084379e0039
Tonita <tonitagjeluci@gmail.com>
2025-08-11 20:07:26 +0200

    Your commit message

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,22 +1,47 @@
 import React from "react";
 import { Section, FeatureCard } from "../styles";
+import smartBudgeting from "../assets/smartBudgeting.jpg";
+import expenseTracking from "../assets/expense-tracking.jpeg";
+import safePrivate from "../assets/safe-private.jpg";
 
 export default function Features() {
   return (
     <Section>
       <h2>Why Choose MoneyMaven?</h2>
       <FeatureCard>
-        <h3>📊 Smart Budgeting</h3>
-        <p>Set and track your financial goals easily.</p>
+
+          <section className="leftSide">
+              <h3>Smart Budgeting</h3>
+              <p>Set and track your financial goals easily. Smart Budgeting helps you plan your spending and saving with clarity.
+                  Easily set financial goals and track your progress in real time.
+                  Stay in control of your money and make confident financial decisions.</p>
+          </section>
+          <section className="pictureHolder">
+              <img src={smartBudgeting} className="cardPicture"></img>
+          </section>
+
+
       </FeatureCard>
-      <FeatureCard>
-        <h3>💰 Expense Tracking</h3>
-        <p>See where your money is going in real time.</p>
-      </FeatureCard>
-      <FeatureCard>
-        <h3>🔒 Secure & Private</h3>
-        <p>Your data is encrypted and protected.</p>
-      </FeatureCard>
+        <FeatureCard>
+                <section className="pictureHolder">
+                    <img src={expenseTracking} className="cardPicture"></img>
+                </section>
+                <section className="reversedRightSide">
+                    <h3>Expense Tracking</h3>
+                    <p>See where your money is going in real time.</p>
+                </section>
+        </FeatureCard>
+        <FeatureCard>
+            <section className="leftSide">
+                <h3>Secure & Private</h3>
+                <p>Your data is encrypted and protected.</p>
+            </section>
+            <section className="pictureHolder">
+                <img src={safePrivate} className="cardPicture"></img>
+            </section>
+
+
+        </FeatureCard>
     </Section>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { HeroContainer, Button } from "../styles";
 export default function Hero() {
   return (
     <HeroContainer>
-      <h1>Take Control of Your Finances with MoneyMaven</h1>
+      <h1>Take control of your finances with MoneyMaven</h1>
       <p>Track, manage, and grow your wealth with ease.</p>
       <Button>Get Started</Button>
     </HeroContainer>
